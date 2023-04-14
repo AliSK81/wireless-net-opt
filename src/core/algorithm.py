1983a6120dd2ba40675986da0226528ac1b00c17
Mohammad_Es <mohammad.es0281@gmail.com>
2023-04-15 00:51:24 +0430

    add or update comments

--- src/core/algorithm.py
+++ src/core/algorithm.py
@@ -13,10 +13,20 @@ class EvolutionaryAlgorithm:
 
     def __init__(self, generation_count: int):
         """
-        Initializes the algorithm with input data from files.
+        Initializes the genetic algorithm with the specified number of generations to evolve.
 
         Args:
-            generation_count (int): Number of generations to evolve.
+            generation_count (int): The number of generations to evolve.
+
+        Attributes:
+            - generation_count (int): The number of generations to evolve.
+            - generations (numpy.ndarray): An array containing the indices of each generation.
+            - sum_of_avg_fitness (numpy.ndarray): An array containing the sum of the average fitness value for each
+             generation.
+            - min_of_avg_fitness (numpy.ndarray): An array containing the minimum average fitness value for each
+             generation.
+            - max_of_avg_fitness (numpy.ndarray): An array containing the maximum average fitness value for each
+            generation.
         """
 
         self.generation_count = generation_count
@@ -25,9 +35,28 @@ class EvolutionaryAlgorithm:
         self.min_of_avg_fitness = np.full(generation_count, np.finfo(np.float64).max)
         self.max_of_avg_fitness = np.full(generation_count, np.finfo(np.float64).min)
 
+    def run_evolve(self, times: int = 1):
+        """
+        Runs the genetic algorithm for a given number of times and displays the results as a plot.
+
+        Args:
+            times (int): Number of times to run the genetic algorithm. Defaults to 1.
+        Returns:
+            None
+        """
+        for _ in range(times):
+            str(self.__evolve())
+
+        Helper.show_plot(x=self.generations, y=self.__get_avg_fitness(times),
+                         y_min=self.min_of_avg_fitness, y_max=self.max_of_avg_fitness,
+                         x_label="generation", y_label="fitness", title="Evolutionary algorithm")
+
     def __evolve(self):
         """
         Evolves the population for a certain number of generations or until a stopping criterion is met.
+
+        Returns:
+        - The best chromosome from the final generation.
         """
         population = Population.initialize()
         population.evaluate_fitness()
@@ -45,28 +74,29 @@ class EvolutionaryAlgorithm:
 
             population.replace(new_generation)
 
-            max_fitness = max(population.chromosomes, key=lambda x: x.fitness).fitness
-            print(max_fitness)
-
-            average = sum([chromosome.fitness for chromosome in population.chromosomes]) / len(population.chromosomes)
-            self.sum_of_avg_fitness[generation] += average
-            self.max_of_avg_fitness[generation] = max(average, self.max_of_avg_fitness[generation])
-            self.min_of_avg_fitness[generation] = min(average, self.min_of_avg_fitness[generation])
+            self.__update_avg_fitness(generation, population)
 
         return population.get_best_chromosome()
 
-    def run_evolve(self, times: int = 1):
-        for _ in range(times):
-            self.__evolve()
+    def __update_avg_fitness(self, generation, population):
+        """
+        Update the statistics for average, maximum and minimum fitness for a given generation.
 
-        self.__show_plot(times)
+        Args:
+            generation (int): The current generation.
+            population (Population): The population whose fitness statistics will be updated.
+        """
+        average = sum([chromosome.fitness for chromosome in population.chromosomes]) / len(population.chromosomes)
+        self.sum_of_avg_fitness[generation] += average
+        self.max_of_avg_fitness[generation] = max(average, self.max_of_avg_fitness[generation])
+        self.min_of_avg_fitness[generation] = min(average, self.min_of_avg_fitness[generation])
 
-    def __show_plot(self, times):
-        average_of_avg_fitness = self.sum_of_avg_fitness / times
-
-        Helper.show_plot(x=self.generations, y=average_of_avg_fitness,
-                         y_min=self.min_of_avg_fitness, y_max=self.max_of_avg_fitness,
-                         x_label="generation", y_label="fitness", title="Evolutionary algorithm")
+    def __get_avg_fitness(self, times):
+        """
+        Returns:
+        - The average fitness of the population over generations.
+        """
+        return self.sum_of_avg_fitness / times
 
     def log(self, name, chromosomes):
         print(name + ':')
--- src/core/gene.py
+++ src/core/gene.py
@@ -5,11 +5,24 @@ from common.config import *
 
 class Gene:
     def __init__(self, location: tuple = None, bandwidth: float = None):
+        """
+        Initializes a new Gene object with the given location and bandwidth.
+
+        Args:
+            location (tuple, optional): A tuple containing the x and y coordinates of the location of the gene. Defaults to None.
+            bandwidth (float, optional): The bandwidth value of the gene. Defaults to None.
+        """
         self.location = location
         self.bandwidth = bandwidth
 
     @staticmethod
     def initialize():
+        """
+        Static method that creates a new Gene object with randomly initialized location and bandwidth.
+
+        Returns:
+            Gene: A new Gene object with randomly initialized location and bandwidth.
+        """
         gene = Gene()
 
         gene.location = (random.uniform(LOCATION_MIN_X, LOCATION_MAX_X), random.uniform(LOCATION_MIN_Y, LOCATION_MAX_Y))
@@ -18,4 +31,13 @@ class Gene:
         return gene
 
     def copy(self):
+        """
+       Creates a copy of the current Gene object.
+
+       Returns:
+           Gene: A new Gene object with the same location and bandwidth as the current Gene object.
+       """
         return Gene(self.location, self.bandwidth)
+
+    def __str__(self):
+        return f"Gene: location={self.location}, bandwidth={self.bandwidth}"
