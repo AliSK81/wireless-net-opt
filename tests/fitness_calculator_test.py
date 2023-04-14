7697a66722f94bf9f9d99a3b342a7068da830f99
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2023-04-14 14:07:57 +0430

    refactor fitness calculator

--- tests/fitness_calculator_test.py
+++ tests/fitness_calculator_test.py
@@ -6,15 +6,26 @@ from operators.fitness.fitness_calculator import FitnessCalculator
 
 class TestFitnessCalculator(unittest.TestCase):
     def test_calculate_fitness(self):
-        gene1 = Gene(location=(1, 2), bandwidth=10)
-        gene2 = Gene(location=(3, 4), bandwidth=30)
+        gene1 = Gene(location=(0.5, 0.75), bandwidth=1000)
+        gene2 = Gene(location=(0.4, 0.8), bandwidth=3000)
         gene3 = gene1
         genes = [gene1, gene2, gene3]
 
-        fitness = FitnessCalculator.calculate_fitness(genes)
-        expected_fitness = 17620
+        calculator = FitnessCalculator(
+            tower_construction_cost=10,
+            tower_maintenance_cost=10,
+            user_satisfaction_levels=[10, 20, 30],
+            user_satisfaction_scores=[100, 200, 300],
+            cities_location=[(0, 0), (0, 1), (1, 0), (1, 1)],
+            cities_population=[10, 20, 30, 40],
+            total_satisfaction_ratio=100,
+            total_cost_ratio=1
+        )
 
-        self.assertEqual(fitness, expected_fitness)
+        actual_fitness = calculator.calculate_fitness(genes)
+        expected_fitness = 1359980
+
+        self.assertEqual(expected_fitness, actual_fitness)
 
 
 if __name__ == '__main__':
